f57ddd4fc6e208cb1f6f643726ae874f39b5ada8
Denis Novikov <novikov.denis.v@gmail.com>
2023-07-18 00:23:12 +0400

    Search page added

--- BulletinBoardProject/announcements/urls.py
+++ BulletinBoardProject/announcements/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import AnnouncementsList, AnnouncementDetail, AnnouncementCreate, AnnouncementUpdate, AnnouncementDelete
+from .views import AnnouncementsList, AnnouncementDetail, \
+    AnnouncementCreate, AnnouncementUpdate, AnnouncementDelete, SearchResultsView
 
 urlpatterns = [
     path('', AnnouncementsList.as_view(), name = 'announcement_list'),
     path('<int:pk>', AnnouncementDetail.as_view(), name = 'announcement_detail'),
     path('create/', AnnouncementCreate.as_view(), name = 'announcement_create'),
-    path('<int:pk>/update/', AnnouncementUpdate.as_view(), name = 'announcement_update' ),
-    path('<int:pk>/delete/', AnnouncementDelete.as_view(), name = 'announcement_delete')
+    path('<int:pk>/update/', AnnouncementUpdate.as_view(), name = 'announcement_update'),
+    path('<int:pk>/delete/', AnnouncementDelete.as_view(), name = 'announcement_delete'),
+    path('search/', SearchResultsView.as_view(), name='post_search'),
 ]
--- BulletinBoardProject/announcements/views.py
+++ BulletinBoardProject/announcements/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.utils import timezone
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -21,6 +24,7 @@ class AnnouncementsList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filterset'] = self.filterset
+        context['time_now'] = timezone.now()
         return context
 
 class AnnouncementDetail(DetailView):
@@ -47,3 +51,20 @@ class AnnouncementDelete(PermissionRequiredMixin, DeleteView):
     model = Announcement
     template_name = 'announcement_delete.html'
     success_url = reverse_lazy('announcement_list')
+
+class SearchResultsView(ListView):
+    model = Announcement
+    template_name = 'search.html'
+    context_object_name = 'search_list'
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = AnnouncementFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        context['time_now'] = datetime.now()
+        return context
